36ce7160b044594f718d2ebf01651f7f1320df6b
git-aftab <mdaftab0726@gmail.com>
2026-03-04 11:15:12 +0530

    completed setting up the project with logger

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("hellow videoTuber!!");
+import logger from "./utils/logger.js";
+import express from "express"
+import morgan from "morgan";
+
+const app = express()
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
+console.log("Hello videoTuber!!");
+logger.info("Hello world")
+logger.warn("Hello world")
+logger.erro("Hello world")
+
