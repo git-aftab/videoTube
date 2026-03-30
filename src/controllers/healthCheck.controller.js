d5c4d151061b9ace850d2f622bf12f70c088514a
git-aftab <mdaftab0726@gmail.com>
2026-03-30 23:57:16 +0530

    setting up the project

---
+++ src/controllers/healthCheck.controller.js
@@ -0,0 +1,9 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+
+const healthCheck = asyncHandler(async (req, res) => {
+  res.status(200).json(new ApiResponse(200, { message: "Server is running" }));
+});
+
+export { healthCheck };
