61ef3c23199c88fd70a9d3783fdb34ed1c73d156
aftab <mdaftab0726@gmail.com>
2026-03-31 13:32:12 +0530

    Completed the Like and Comment models

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import logger from "./utils/logger.js";
 
 const app = express()
 
-app.use(express.json({limit: "10mb"}))
+app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
