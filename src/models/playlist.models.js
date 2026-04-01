44abfb0d4750eb9a736ce4259bf5cab295b04e12
aftab <mdaftab0726@gmail.com>
2026-04-01 13:40:51 +0530

    completed playlist, subscription, tweet models

---
+++ src/models/playlist.models.js
@@ -0,0 +1,21 @@
+import mongoose, {Schema} from "mongoose";
+
+const playlistSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+    },
+    videos:[{
+        type: Schema.Types.ObjectId,
+        ref: "Videos"
+    }], // array of videos
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+},{timestamps: true})
+
+export const Playlist = mongoose.model("Playlist",playlistSchema);
