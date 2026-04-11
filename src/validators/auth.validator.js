c979aa8859d451fff05097650f865056114f287e
aftab <mdaftab0726@gmail.com>
2026-04-11 14:28:59 +0530

    completed the auth.validator.js

---
+++ src/validators/auth.validator.js
@@ -0,0 +1,72 @@
+import { body } from "express-validator";
+
+const registerValidator = () => {
+  [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+
+    body("username")
+      .trim()
+      .notEmpty()
+      .withMessage("username is required")
+      .isLowercase()
+      .withMessage("username must be in lowercase")
+      .isLength({ min: 3 })
+      .withMessage("username must have atleast 3 characters"),
+
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("Password is required")
+      .isLength({ min: 6 }),
+  ];
+};
+
+const loginValidator = () => {
+  [
+    body("email").trim().notEmpty().withMessage("Email is required"),
+    body("username").trim().notEmpty().withMessage("username is required"),
+    body("password").trim().notEmpty().withMessage("Password is required"),
+  ];
+};
+
+const changeCurrentPasswordValidator = () => {
+  [
+    body("oldPassword")
+      .trim()
+      .notEmpty()
+      .withMessage("Old Password is required"),
+    body("newPassword")
+      .trim()
+      .notEmpty()
+      .withMessage("New Password is required"),
+  ];
+};
+
+const userForgotPasswordValidator = () => {
+  return [
+    body("email")
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is Invalid"),
+  ];
+};
+
+const resetForgotPasswordValidator = () => {
+  return [
+    body("newPassword").trim().notEmpty().withMessage("Password is required"),
+  ];
+};
+
+export {
+  registerValidator,
+  loginValidator,
+  changeCurrentPasswordValidator,
+  userForgotPasswordValidator,
+  resetForgotPasswordValidator,
+};
