7843885106e7fc53a261ae06ad6983521e7eb197
aftab <mdaftab0726@gmail.com>
2026-04-22 20:42:13 +0530

    added createPlaylist and getUserPlaylist

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,18 +1,46 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
+import { Playlist } from "../models/playlist.models.js";
 
 // controller
 const createPlaylist = asyncHandler(async (req, res) => {
+  //TODO: create playlist
   const { name, description } = req.body;
 
-  //TODO: create playlist
+  if (!name.trim()) {
+    throw new ApiError(400, "Playlist Name is required");
+  }
+
+  const playlist = await Playlist.create({
+    name: name,
+    description: description || "",
+    owner: req.user._id,
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, playlist, "Playlist Created Successfully"));
 });
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
-  const { userId } = req.params;
   //TODO: get user playlists
+  const { userId } = req.params;
+
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid User ID");
+  }
+
+  const playlists = await Playlist.find({ owner: userId })
+    .select("name description videos createdAt")
+    .sort({ createdAt: -1 });
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, playlists, "User playlists fetched successfully"),
+    );
 });
 
 const getPlaylistById = asyncHandler(async (req, res) => {
