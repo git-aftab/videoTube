a5679a8ec7173e5804b757a758624cfb292e2c37
aftab <mdaftab0726@gmail.com>
2026-04-23 13:17:33 +0530

    added comment routes

---
+++ src/routes/comment.routes.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { verifyOwnerShip } from "../middlewares/ownership.middleware.js";
+import {
+  addCommentValidator,
+  updateCommentValidator,
+} from "../validators/comment.validators.js";
+import {
+  addComment,
+  updateComment,
+  deleteComment,
+  getVideoComments,
+} from "../controllers/comment.controller.js";
+import { Comment } from "../models/comment.models.js";
+
+const router = Router();
+
+router.route("/videoId").get(getVideoComments).post(verifyJWT, addComment);
+
+router
+  .route("/videoId/commentId")
+  .post(verifyJWT, verifyOwnerShip(Comment, "commentId"), deleteComment)
+  .patch(verifyJWT, verifyOwnerShip(Comment, "commentId"), updateComment);
