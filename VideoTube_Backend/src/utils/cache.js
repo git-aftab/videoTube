a9c671cf38f01caf2b800e19ad90924a85e5da0a
aftab <mdaftab0726@gmail.com>
2026-05-05 10:40:25 +0530

    setting up caching services

---
+++ VideoTube_Backend/src/utils/cache.js
@@ -0,0 +1,30 @@
+import redis from "redis";
+
+export const getCache = async (key) => {
+  try {
+    const data = await redis.get(key);
+    return data ? JSON.parse(data) : null;
+  } catch (error) {
+    console.error("Cache GET error:", error);
+    return null;
+  }
+};
+
+export const setCache = async (key, value, ttl = 60) => {
+  try {
+    await redis.set(key, JSON.stringify(value), "EX", ttl);
+  } catch (error) {
+    console.error("Cache SET error:", err);
+  }
+};
+
+export const deleteCache = async (pattern) => {
+  try {
+    const keys = await redis.keys(pattern);
+    if (keys.length) {
+      await redis.del(keys);
+    }
+  } catch (error) {
+    console.error("Cache DELETE error:", error);
+  }
+};
