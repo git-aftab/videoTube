539418628afdfd71f690ddab466f2959f51982be
aftab <mdaftab0726@gmail.com>
2026-05-21 00:12:21 +0530

    added the constants for rag services

--- VideoTube_Backend/src/constants/Rag.constants.js
+++ VideoTube_Backend/src/constants/Rag.constants.js
@@ -1,9 +1,16 @@
-export const EMBEDDING_MODEL = "";
+export const EMBEDDING_MODEL = "jina-embeddings-v2-base-en";
 
-export const CHAT_MODEL = "";
+export const CHAT_MODEL = "llama-3.3-70b-versatile";
 
 export const EMBDDING_DIMENSION = 768;
 
 export const CHUNK_SIZE = 400;
 
 export const CHUNK_OVERLAP = 80;
+
+export const RETRIEVEL_COUNT = 8;
+
+export const FINAL_CONTEXT_COUNT = 3;
+
+export const GROQ_BASE_URL = "https://api.groq.com/openai/v1";
+
