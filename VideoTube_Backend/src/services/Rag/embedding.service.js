c9fcecd1542d5c816093b84e8668ff56c0202602
aftab <mdaftab0726@gmail.com>
2026-05-22 20:19:11 +0530

    added embedding service using jina embedding api

---
+++ VideoTube_Backend/src/services/Rag/embedding.service.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import {
+  EMBEDDING_MODEL,
+  EMBDDING_DIMENSION,
+} from "../../constants/Rag.constants.js";
+
+export const generateEmbedding = async (text, task = "retrieval.passage") => {
+  try {
+    const res = await axios.post(
+      "https://api.jina.ai/v1/embeddings",
+      {
+        model: EMBEDDING_MODEL,
+        task,
+        normalized: true,
+        input: [text],
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.JINA_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+      },
+    );
+
+    console.log(res.data);
+    return res.data.data[0].embedding;
+  } catch (error) {
+    console.error(
+      "Embedding generation failed",
+      error.res?.data || error.message,
+    );
+    throw error;
+  }
+};
