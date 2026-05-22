06085c11ac6934d55aa86bf0e29a3c3027ce5c02
aftab <mdaftab0726@gmail.com>
2026-05-22 19:51:12 +0530

    Completed setting up the Qdrant DB

--- VideoTube_Backend/src/services/Rag/qdrant.service.js
+++ VideoTube_Backend/src/services/Rag/qdrant.service.js
@@ -1,6 +1,32 @@
-import {qdrant} from "../../db/qdrant.js"
-import crypto from  "crypto"
+import { qdrant } from "../../db/qdrant.js";
+import crypto from "crypto";
 
-export const storeEmbeddings = async({
-    videoId, chunkText, embedding, 
-})
+export const storeEmbeddings = async ({
+  videoId,
+  chunkText,
+  embedding,
+  chunkIndex,
+}) => {
+  try {
+    await qdrant.upsert("video-transcripts", {
+      wait: true,
+      points: [
+        {
+          id: crypto.randomUUID(),
+          vector: embedding,
+
+          payload: {
+            videoId,
+            chunkText,
+            chunkIndex,
+          },
+        },
+      ],
+    });
+
+    console.log(`Stored chunk ${chunkIndex} in Qdrant`);
+  } catch (error) {
+    console.error("Qdrand store failed:", error.message);
+    throw error;
+  }
+};
