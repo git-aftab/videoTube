c5c4e65e1cb04f1df565f2635fd138e3f43be1fe
aftab <mdaftab0726@gmail.com>
2026-06-06 19:35:00 +0530

    Starting frontend: downloaded the req packages, in backend still some routes to be tested and will be done parallelly

---
+++ VideoTube_Frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="items-center flex justify-center bg-black h-screen">
+      <div className="text-8xl text-purple-500 ">
+        VideoTube
+      </div>
+    </div>
+  );
+};
+
+export default App;
