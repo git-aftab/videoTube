74c4bb8256c7e7fe5e3540a7ea8107c089dcda3c
aftab <mdaftab0726@gmail.com>
2026-06-29 20:51:09 +0530

    filler: progressing in the homePage

---
+++ VideoTube_Frontend/src/components/ui/FilterBar.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+const FilterBar = () => {
+  const [selected, setSelected] = useState('All')
+
+  const categories = ["All", "Music", "Gaminig", "Programming", "Sports"];
+
+  return (
+    <h1 className="w-full h-10 flex gap-3 items-center ">
+      {categories.map((cat) => (
+        <button
+          onClick={() => setSelected(cat)}
+          className={`py-2 px-3 text-xl font-bold rounded-xl hover:bg-accent ${selected === cat? 'bg-accent text-white' : 'border border-border'}`}
+        >
+          {cat}
+        </button>
+      ))}
+    </h1>
+  );
+};
+
+export default FilterBar;
--- VideoTube_Frontend/src/pages/Home.tsx
+++ VideoTube_Frontend/src/pages/Home.tsx
@@ -1,70 +1,49 @@
 import { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
-import VideoCards from "../components/videoCards/VideoCards";
-import { FaArrowTrendUp } from "react-icons/fa6";
-import { FaHeart } from "react-icons/fa";
+import VideoCards from "../components/ui/VideoCards";
+import FilterBar from "../components/ui/FilterBar";
+import { GiHamburgerMenu } from "react-icons/gi";
+
 
 const Home = () => {
   const [videos, setVideos] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  return (
-    <main className="relative bg-(--bg-primary) py-6 text-(--text-primary) px-6 space-y-6">
-      {/* <aside className="hidden sm:flex flex-col ">
 
-      </aside> */}
-      <div className="flex flex-col justify-center mb-6">
-        <h1 className=" mb-3 text-3xl font-bold tracking-tight flex items-center border-b border-(--accent-soft)">
-          Currently Trendings{" "}
-          <span className="">
-            <FaArrowTrendUp size={24} className="text-accent" />
-          </span>
-        </h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
-          <div className="">
-            <VideoCards />
-          </div>
-          <div className="">
-            <VideoCards />
-          </div>
-          <div className="">
-            <VideoCards />
-          </div>
-          <div className="">
-            <VideoCards />
-          </div>
+  return (
+    <main className="relative grid md:grid-cols-7  bg-(--bg-primary) py-6 text-(--text-primary) px-6 space-y-6">
+      <aside className="hidden lg:flex flex-col col-span-1 gap-x-3">
+        <div className="">
+          <GiHamburgerMenu size={24} />
         </div>
-      </div>
-      <div className="flex flex-col justify-center mb-6">
-        <h1 className="text-(--text-primary) text-3xl font-bold tracking-tight flex items-center border-b border-(--accent-soft)">
-          Recomendations{" "}
-          <FaHeart size={24} fill="#7c3aed" color="#7c3aed" className="" />
-        </h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">  
-          <div className="">
-            <VideoCards/>
-          </div>
-          <div className="">
-            <VideoCards/>
-          </div>
-          <div className="">
-            <VideoCards/>
-          </div>
-          <div className="">
-            <VideoCards/>
-          </div>
-          <div className="">
-            <VideoCards/>
-          </div>
-          <div className="">
-            <VideoCards/>
-          </div>
-          <div className="">
-            <VideoCards/>
-          </div>
-          <div className="">
-            <VideoCards/>
+        <div className="flex flex-col gap-y-3">
+          <h1 className="font-bold hover:bg-accent border border-border rounded-lg py-2 px-3">HOME</h1>
+          <h1 className="font-bold hover:bg-accent border border-border rounded-lg py-2 px-3">Watch HISTORY</h1>
+          <h1 className="font-bold hover:bg-accent border border-border rounded-lg py-2 px-3">SUBSCRIPTIONS</h1>
+          <h1 className="font-bold hover:bg-accent border border-border rounded-lg py-2 px-3">YOU</h1>
+          <h1 className="text-sm border-t border-(--text-muted)"></h1>
+          <h1 className="font-bold hover:bg-accent border border-border rounded-lg py-2 px-3">REPORT</h1>
+        </div>
+      </aside>
+      <div className=" relative col-span-6 flex flex-col gap-y-3 ">
+        <div className="">
+          <FilterBar />
+        </div>
+        <div className="flex flex-col justify-center mb-6 ">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-10">
+            <VideoCards />
+            <VideoCards />
+            <VideoCards />
+            <VideoCards />
+            <VideoCards />
+            <VideoCards />
+            <VideoCards />
+            <VideoCards />
+            <VideoCards />
+            <VideoCards />
+            <VideoCards />
+            <VideoCards />
           </div>
         </div>
       </div>
